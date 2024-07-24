1a057bf0d358d9f41f43688efb6911465d700a40
TCCJOAORAYS <tccjaoraysa@gmail.com>
2024-07-24 11:44:49 -0300

    é isso

--- Personal/wwwroot/js/site.js
+++ Personal/wwwroot/js/site.js
@@ -1,4 +1,93 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
 
-// Write your JavaScript code.
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+/* Slides */
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 4000,  
+    }, 
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
+
+let isScrolling;
+let bodyWidth = document.body.scrollWidth;
+const header = document.querySelector('.header');
+
+
+
+window.addEventListener('scroll', () => {
+const currentScrollTop = window.scrollY;
+
+        if (currentScrollTop > 0) {
+            header.style.opacity = '0.1';
+
+        } else {
+            header.style.opacity = '1';
+        }
+
+        clearTimeout(isScrolling);
+
+        isScrolling = setTimeout(() => {
+            header.style.opacity = '1';
+        }, 500);
+     });
