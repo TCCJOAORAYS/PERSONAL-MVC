4cfe45fb72ea542b1fd8dde40b481ad882889518
TCCJOAORAYS <tccjaoraysa@gmail.com>
2024-10-18 11:29:10 -0300

    footerrrr

--- Personal/Migrations/20241018141211_InitialCreate.Designer.cs
+++ Personal/Migrations/20241018141211_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Personal.Data;
 namespace Personal.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20241016132924_InitialCreate")]
+    [Migration("20241018141211_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
