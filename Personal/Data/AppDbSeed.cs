d23b4aee4f970b1ee8ca20f16a88e9be33acbafb
TCCJOAORAYS <tccjaoraysa@gmail.com>
2024-11-27 11:07:32 -0300

    acabou oficial

--- Personal/Data/AppDbSeed.cs
+++ Personal/Data/AppDbSeed.cs
@@ -30,7 +30,7 @@ public class AppDbSeed
                 Nome = "Teste de tug: Timed up and Go Test",
                 NomeAux = "Teste De Tug",
                 Titulo = "Avaliação funcional: Timed Up and Go test",
-                Descricao = "Para realizar o teste como descrito na versão original, o Testado é cronometrado enquanto se levanta de uma cadeira (aproximadamente 46cm), caminha em uma linha reta de 3metros de distância (em um ritmo confortável e seguro), vira, caminha de volta e senta-se sobre a cadeira novamente.",
+                Descricao = "Para realizar o teste como descrito na versão original, o Testado é cronometrado enquanto se levanta de uma cadeira (aproximadamente 46cm), caminha em uma linha reta de 3 metros de distância (em um ritmo confortável e seguro), vira, caminha de volta e senta-se sobre a cadeira novamente.",
                 Tipo = "Segundo Teste Rápido",
                 Imagem = "https://raw.githubusercontent.com/TCCJOAORAYS/IMAGENS/main/img/testes/tug.png",
                 ImagemAux = "https://raw.githubusercontent.com/TCCJOAORAYS/IMAGENS/main/img/exercicios/doreslocais.jpg",
--- Personal/Migrations/20241127132130_InitialCreate.Designer.cs
+++ Personal/Migrations/20241127132130_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Personal.Data;
 namespace Personal.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20241126113913_InitialCreate")]
+    [Migration("20241127132130_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
